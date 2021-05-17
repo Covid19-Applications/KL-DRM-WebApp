d858b7cfdb7e1138559029cc77fb0ba04527376f
Shaharyaar7 <shaharyaarkutchi7@gmail.com>
2021-05-17 22:09:01 +0530

    created modules

---
+++ govt.php
@@ -0,0 +1,128 @@
+<?php include('security.php'); ?>
+<?php include('includes/header.php');?>
+<?php include('includes/navbar.php');?>
+
+
+<br><br><br><br>
+
+  
+<style type="text/css">
+  
+.button{
+  background: #3498db;
+    color: #fff;
+    border-radius: 50px;
+    margin: 0 15px;
+    padding: 10px 30px; 
+}
+
+#services .box {
+    padding: 50px 20px;
+    margin-bottom: 50px;
+    text-align: center;
+    border: 1px solid #e6e6e6;
+    height: 170px;
+    position: relative;
+    background: #fafafa;
+}
+
+.team .member{
+  height: 100px;
+}
+  
+</style>
+    <!-- bradcam_area_start  -->
+    <div class="bradcam_area breadcam_bg ">
+        <div class="container">
+          <br><br>
+            <div class="row">
+                <div class="col-lg-4">
+                    <div class="bradcam_text">
+                        <img src="img/kerala/logo.png" class="img-fluid" style="height: 150px;">
+                        <h1>Government Services</h1>
+                    </div>
+                </div>
+                <div class="col-lg-8">
+                    <div class="bradcam_text">
+                      <br>
+                        <img src="img/kerala/banner1.png" class="img-fluid">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- bradcam_area_end  -->
+
+
+    <!-- ======= Team Section ======= -->
+    <section id="team" class="team">
+      <div class="container">
+
+        <div class="section-title" data-aos="fade-in" data-aos-delay="100">
+          <h2>Covid & Govt. Services</h2>
+          <p>Digital Kerala</p>
+        </div>
+
+        <div class="row">
+
+          <div class="col-lg-3">
+            <div class="member" data-aos="fade-up">
+              <div class="member-info">
+                <h4><a href="https://dashboard.kerala.gov.in/ck-view-public.php">Covid19 - Community Kitchen</a></h4>
+                
+              </div>
+            </div>
+            <div class="member" data-aos="fade-up" style="height: 100px;">
+              <div class="member-info">
+                <h4> <a href="https://kerala.gov.in/service">UTILITY PAYMENT</a></h4>
+              </div>
+            </div>
+          </div> 
+                    <div class="col-lg-3">
+            <div class="member" data-aos="fade-up">
+              <div class="member-info">
+                <h4><a href="https://dashboard.kerala.gov.in/destitute-rehabilitation.php">Covid19 - Destitute Rehabilitation</a></h4>
+                
+              </div>
+            </div>
+            <div class="member" data-aos="fade-up" style="height: 100px;">
+              <div class="member-info">
+                <h4><a href="https://kerala.gov.in/service"> PERMIT/LICENSES</a></h4>
+              </div>
+            </div>
+          </div> 
+                    <div class="col-lg-3">
+            <div class="member" data-aos="fade-up">
+              <div class="member-info">
+                <h4><a href="https://dashboard.kerala.gov.in/volunteers.php">Covid19 - Social Volunteers Force</a></h4>
+                
+              </div>
+            </div>
+            <div class="member" data-aos="fade-up" style="height: 100px;">
+              <div class="member-info">
+                <h4><a href="https://kerala.gov.in/service"> WELFARE SCHEMES</a></h4>
+              </div>
+            </div>
+          </div> 
+          <div class="col-lg-3"> 
+              <div class="member" data-aos="fade-up"> 
+              <div class="member-info">
+                <h4><a href="https://dashboard.kerala.gov.in/psycosocial.php">Covid19 - Psychosocial Support</a></h4>
+               
+              </div>
+            </div>
+             <div class="member" data-aos="fade-up" >
+              <div class="member-info">
+                <h4><a href="https://kerala.gov.in/service">TAX PAYMENTS</a></h4>
+                
+              </div>
+            </div>
+          </div> 
+
+        </div>
+
+      </div>
+    </section><!-- End Team Section -->
+
+<?php include('includes/scripts.php');?>
+<?php include('includes/footer.php');?>
---
+++ index.php
@@ -0,0 +1,453 @@
+<?php include('security.php'); ?>
+<?php include('includes/header.php');?>
+<?php include('includes/navbar.php');?>
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero">
+    <div id="heroCarousel" class="carousel slide carousel-fade" data-ride="carousel">
+
+      <ol class="carousel-indicators" id="hero-carousel-indicators"></ol>
+
+      <div class="carousel-inner" role="listbox">
+
+        <!-- Slide 1 -->
+        <div class="carousel-item active" style="background-image: url(img/kerala/bg.jpg)">
+          <div class="carousel-container">
+            <div class="container">
+              <h2 class="animate__animated animate__fadeInDown">Welcome to <span>Kerala</span></h2>
+            </div>
+          </div>
+        </div>
+
+        <!-- Slide 2 -->
+        <div class="carousel-item" style="background-image: url(img/kerala/bg1.jpg)">
+          <div class="carousel-container">
+            <div class="container">
+            </div>
+          </div>
+        </div>
+
+        <!-- Slide 3 -->
+        <div class="carousel-item" style="background-image: url(img/kerala/bg2.jpg)">
+          <div class="carousel-container">
+            <div class="container">
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+      <a class="carousel-control-prev" href="#heroCarousel" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon icofont-simple-left" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+
+      <a class="carousel-control-next" href="#heroCarousel" role="button" data-slide="next">
+        <span class="carousel-control-next-icon icofont-simple-right" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+
+    </div>
+  </section><!-- End Hero -->
+
+  <main id="main">
+
+    <!-- ======= About Section ======= -->
+    <section id="about" class="about">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+          <h2>About</h2>
+          <p>About Us</p>
+        </div>
+
+        <div class="row content">
+          <div class="col-lg-6">
+            <img src="img/kerala/map.png" class="img-fluid" style="height: 500px;">
+           
+          </div>
+          <div class="col-lg-6 pt-4 pt-lg-0">
+            <p>
+              Kerala is situated in the southwestern end of the Indian subcontinent. Kerala has a long history of art and cultural heritage and foreign trade with other countries. The state with the highest literacy rate in India, is noted for its achievements in education, health, gender equality, social justice, law and order. In addition to these, the state has the lowest infant mortality rate in the country.
+              <br><br> 
+              Kerala lies between the Arabian Sea in the West and the Western Ghats (Sahyadris) in the East with an area of 38863 sq km. It is one of the five states in the linguistic-cultural area known as South India. The neighbouring states of Kerala are Tamil Nadu and Karnataka. Mayyazhi (Mahe / Mahe) is a part of Pondicherry (Puducherry) and lies within Kerala. Though the Lakshadweeps in the Arabian Sea is part of the Union Territories, it has a close alliance with the linguistic and cultural heritage of Kerala.
+            </p>
+             <ul>
+              <li><i class="ri-check-double-line"></i><strong>Governor:</strong> Arif Mohammad Khan Trending</li>
+              <li><i class="ri-check-double-line"></i><strong>Population:</strong> 3.48 crores (2012)</li>
+              <li><i class="ri-check-double-line"></i><strong>Capital:</strong> Thiruvananthapuram</li>
+              <li><i class="ri-check-double-line"></i><strong>Chief minister:</strong> Pinarayi Vijayan</li>
+            </ul>
+            <a href="https://kerala.gov.in/about-kerala" class="btn-learn-more">Know More</a>
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End About Section -->
+
+    <!-- ======= Counts Section ======= -->
+    <section id="counts" class="counts">
+      <div class="container" data-aos="fade-up">
+
+        <div class="row no-gutters">
+
+          <div class="col-lg-3 col-md-6 d-md-flex align-items-md-stretch">
+            <div class="count-box">
+               <img src="img/icons/virus.png">
+              <span data-toggle="counter-up" id="StateConfirmed"></span>
+              <p><strong>Covid19 Confirmed Cases</strong></p>
+           
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-6 d-md-flex align-items-md-stretch">
+            <div class="count-box">
+              <img src="img/icons/virus.png">
+              <span data-toggle="counter-up" id="StateActive"></span>
+              <p><strong>Covid19 Active Cases</strong></p>
+            
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-6 d-md-flex align-items-md-stretch">
+            <div class="count-box">
+              <img src="img/icons/virus.png">
+              <span data-toggle="counter-up" id="StateRecovered"></span>
+              <p><strong>Covid19 Recovered Cases</strong></p>
+            
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-6 d-md-flex align-items-md-stretch">
+            <div class="count-box">
+              <img src="img/icons/virus.png">
+              <span data-toggle="counter-up" id="StateDeath"></span>
+              <p><strong>Covid19 Death Cases</strong></p>
+            
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Counts Section -->
+
+
+    <!-- ======= Cta Section ======= -->
+    <section id="cta" class="cta">
+      <div class="container" data-aos="zoom-in">
+
+        <div class="text-center">
+          <h3>Coronavirus</h3>
+          <p>Coronavirus disease (COVID-19) is an infectious disease caused by a newly discovered coronavirus.
+Most people infected with the COVID-19 virus will experience mild to moderate respiratory illness and recover without requiring special treatment.Older people, and those with underlying medical problems like cardiovascular disease, diabetes, chronic respiratory disease, and cancer are more likely to develop serious illness.<br><br>
+The COVID-19 virus spreads primarily through droplets of saliva or discharge from the nose when an infected person coughs or sneezes, so it’s important that you also practice respiratory etiquette (for example, by coughing into a flexed elbow).
+At this time, there are no specific vaccines or treatments for COVID-19. However, there are many ongoing clinical trials evaluating potential treatments. WHO will continue to provide updated information as soon as clinical findings become available.</p>
+        </div>
+
+      </div>
+    </section><!-- End Cta Section -->
+
+    <br><br><br>
+
+    <!-- ======= Features Section ======= -->
+    <section id="services" class="services">
+      <div class="container" data-aos="fade-up">
+
+         <div class="section-title">
+          <h2>Coronavirus Symptoms</h2>
+          <p>Check the symptoms</p>
+        </div>
+
+        <div class="row">
+         <div class="col-lg-4 col-md-6 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="100">
+            <div class="icon-box">
+              <div class="icon">
+              <img src="img/icons/cold.png">
+            </div>
+              <h4>Fever</h4>
+              <p>A fever is a body temperature that is higher than normal. A normal temperature can vary from person to person,but it is usually around 98.6F. </p>
+            </div>
+         </div>   
+         <div class="col-lg-4 col-md-6 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="100">
+            <div class="icon-box">
+              <div class="icon">
+              <img src="img/icons/education.png">
+            </div>
+              <h4>Tiredness</h4>
+              <p>Feeling overtired, with low energy and a strong desire to sleep that interferes with normal daily activities.</p>
+            </div>
+          </div>  
+            <div class="col-lg-4 col-md-6 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="100">
+            <div class="icon-box">
+              <div class="icon">
+              <img src="img/icons/avatar.png">
+            </div>
+              <h4>Dry Cough</h4>
+              <p>A sudden, forceful hacking sound to release air and clear an irritation in the throat or airway.</p>
+            </div>
+          </div>  
+            <div class="col-lg-4 col-md-6 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="100">
+            <div class="icon-box">
+              <div class="icon">
+              <img src="img/icons//healthcare-and-medical (2).png">
+            </div>
+              <h4>Aches and Pains</h4>
+              <p>Muscle pain is most often related to tension, overuse, or muscle injury from exercise or hard physical work.</p>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-6 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="100">
+            <div class="icon-box">
+              <div class="icon">
+              <img src="img/icons/discharge.png">
+            </div>
+              <h4>Nasal Congestion</h4>
+              <p>Nasal passages swollen with excess fluid and mucus, may be triggered by infection, tobacco smoke or perfume.</p>
+            </div>
+          </div>  
+            <div class="col-lg-4 col-md-6 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="100">
+            <div class="icon-box">
+              <div class="icon">
+              <img src="img/icons/discharge (5).png">
+            </div>
+              <h4>Runny Nose</h4>
+              <p>Excess drainage, ranging from a clear fluid to thick mucus, from the nose and nasal passages.</p>
+            </div>
+          </div>  
+         
+        </div>
+
+      </div>
+    </section><!-- End Features Section -->
+
+
+
+    <!-- ======= Portfolio Section ======= -->
+    <section id="portfolio" class="portfolio">
+      <div class="container" data-aos="fade-up">
+       
+        <div class="section-title">
+          <h2>Our Services</h2>
+          <p>Covid19 Services</p>
+        </div>
+
+        <div class="row" data-aos="fade-up" data-aos-delay="100">
+          <div class="col-lg-12 d-flex justify-content-center">
+            <ul id="portfolio-flters">
+              <li data-filter="*" class="filter-active">All</li>
+              <li data-filter=".filter-app">Health Care</li>
+              <li data-filter=".filter-card">Disaster Management</li>
+              <li data-filter=".filter-web">Education & Skillng</li>
+            </ul>
+          </div>
+        </div>
+
+        <div class="row portfolio-container" data-aos="fade-up" data-aos-delay="200">
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+            <img src="img/kerala/center.jpg" class="img-fluid" alt="" style="height: 220px; width: 420px;">
+            <div class="portfolio-info">
+              <h4>Public Health Care Center</h4>
+              <a href="#" class="details-link" title="More Details"></a>
+            </div>
+          </div>
+
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+            <img src="img/home/medical.png" class="img-fluid" alt="" style="height: 220px; width: 420px;">
+            <div class="portfolio-info">
+              <h4>Medical</h4>
+              <a href="#" class="details-link" title="More Details"></a>
+            </div>
+          </div>
+
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+            <img src="img/home/blood.png" class="img-fluid" alt="" style="height: 220px; width: 420px;">
+            <div class="portfolio-info">
+              <h4>Blood Bank</h4>
+              <a href="#" class="details-link" title="More Details"></a>
+            </div>
+          </div>
+
+           <div class="col-lg-4 col-md-6 portfolio-item filter-card">
+            <img src="img/home/donate.png" class="img-fluid" alt="" style="height: 220px; width: 420px;">
+            <div class="portfolio-info">
+              <h4>Donations</h4>
+              <a href="#" class="details-link" title="More Details"></a>
+            </div>
+          </div>
+
+             <div class="col-lg-4 col-md-6 portfolio-item filter-card">
+            <img src="img/home/volunteer.jpg" class="img-fluid" alt="" style="height: 220px; width: 420px;">
+            <div class="portfolio-info">
+              <h4>Volunteers</h4>
+              <a href="#" class="details-link" title="More Details"></a>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-card">
+            <img src="img/kerala/logo.png" class="img-fluid" alt="" style="height: 220px; width: 420px;">
+            <div class="portfolio-info">
+              <h4>Government Services</h4>
+              <a href="#" class="details-link" title="More Details"></a>
+            </div>
+          </div>
+
+           <div class="col-lg-4 col-md-6 portfolio-item filter-card">
+            <img src="img/home/mask.jpg" class="img-fluid" alt="" style="height: 220px; width: 420px;">
+            <div class="portfolio-info">
+              <h4>Movement Pass</h4>
+              <a href="#" class="details-link" title="More Details"></a>
+            </div>
+          </div>
+          
+         <div class="col-lg-4 col-md-6 portfolio-item filter-web">
+            <img src="img/home/hr.png" class="img-fluid" alt="" style="height: 220px; width: 420px;">
+            <div class="portfolio-info">
+              <h4>HR Recruitment</h4>
+              <a href="#" class="details-link" title="More Details"></a>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-web">
+            <img src="img/home/courses.jpg" class="img-fluid" alt="" style="height: 220px; width: 420px;">
+            <div class="portfolio-info">
+              <h4>Online Vocational Training Courses</h4>
+              <a href="#" class="details-link" title="More Details"></a>
+            </div>
+          </div>
+
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+            <img src="img/home/msme.jpg" class="img-fluid" alt="" style="height: 220px; width: 420px;">
+            <div class="portfolio-info">
+              <h4>MSME Products</h4>
+              <a href="#" class="details-link" title="More Details"></a>
+            </div>
+          </div>
+
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-card">
+            <img src="img/home/courses.png" class="img-fluid" alt="" style="height: 220px; width: 420px;">
+            <div class="portfolio-info">
+              <h4>Essential Workers</h4>
+              <a href="#" class="details-link" title="More Details"></a>
+            </div>
+          </div>
+
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-card">
+            <img src="img/home/donation.jpg" class="img-fluid" alt="" style="height: 220px; width: 420px;">
+            <div class="portfolio-info">
+              <h4>Food Supply</h4>
+              <a href="#" class="details-link" title="More Details"></a>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Portfolio Section -->
+
+    <!-- ======= Team Section ======= -->
+    <section id="team" class="team">
+      <div class="container">
+
+        <div class="section-title" data-aos="fade-in" data-aos-delay="100">
+          <h2>To Flatten The Curve & Break the Chain</h2>
+          <p>STAY HOME, STAY SAFE</p>
+        </div>
+
+        <div class="row">
+
+          <div class="col-lg-3">
+            <div class="member" data-aos="fade-up">
+              <div class="member-info">
+                <h4> WASH HANDS OFTEN</h4>
+                
+              </div>
+            </div>
+            <div class="member" data-aos="fade-up" style="height: 100px;">
+              <div class="member-info">
+                <h4> COVER YOUR COUGH</h4>
+              </div>
+            </div>
+          </div> 
+          <div class="col-lg-3"> 
+              <div class="member" data-aos="fade-up"> 
+              <div class="member-info">
+                <h4>KEEP A SAFE DISTANCE</h4>
+               
+              </div>
+            </div>
+             <div class="member" data-aos="fade-up" >
+              <div class="member-info">
+                <h4>SICK? CALL THE HELPLINE</h4>
+                
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-6">
+            <img src="img/home/banner.png" class="img-fluid">
+          </div>  
+
+        </div>
+
+      </div>
+    </section><!-- End Team Section -->
+
+
+
+  </main><!-- End #main -->
+
+
+<script type="text/javascript">
+  
+$.ajax({
+    url:"https://api.covid19india.org/v2/state_district_wise.json",
+    type:"GET",
+    dataType:"JSON",
+    success : function(covid_data) {
+
+     //console.log(covid_data);
+     var newArray = covid_data.filter(function (el) {
+
+      return el.state === 'Kerala';
+
+    });
+
+    //console.log(newArray);
+
+    Array.prototype.sum = function (prop) {
+    var total = 0
+    for ( var i = 0, _len = this.length; i < _len; i++ ) {
+        total += this[i][prop]
+    }
+    return total
+    }
+
+    var StateConfirmed = newArray[0].districtData.sum("confirmed");
+    document.getElementById("StateConfirmed").innerHTML = StateConfirmed;
+
+    var StateActive = newArray[0].districtData.sum("active");
+    document.getElementById("StateActive").innerHTML = StateActive;
+
+    var StateDeath = newArray[0].districtData.sum("deceased");
+    document.getElementById("StateDeath").innerHTML = StateDeath;
+
+    var StateRecovered = newArray[0].districtData.sum("recovered");
+    document.getElementById("StateRecovered").innerHTML = StateRecovered;
+
+    // var MainStateConfirmed = newArray[0].districtData.sum("confirmed");
+    // document.getElementById("MainStateConfirmed").innerHTML = "Covid19 Cases - " + (MainStateConfirmed) ;
+}
+
+});
+
+
+</script>
+
+<?php include('includes/scripts.php');?>
+<?php include('includes/footer.php');?>
